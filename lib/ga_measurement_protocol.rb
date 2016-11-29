0e007592c3f098130873c3d014b8a3c4be136ba7
Naofumi Fujii <naofumi.fujii@gmail.com>
2016-11-29 06:14:47 +0000

    add minimal implementation

--- lib/ga_measurement_protocol.rb
+++ lib/ga_measurement_protocol.rb
@@ -1,5 +1,24 @@
-require "ga_measurement_protocol/version"
+require 'ga_measurement_protocol/version'
+require 'faraday'
+require 'json'
 
 module GaMeasurementProtocol
   # Your code goes here...
+  class Client
+    attr_reader :conn
+    def initialize
+      url =
+        'https://www.google-analytics.com'
+
+      @conn = Faraday.new(url: url) do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+    end
+
+    def post(request)
+      JSON.parse(conn.post('/debug/collect', request).body)
+    end
+  end
 end
--- spec/ga_measurement_protocol_spec.rb
+++ spec/ga_measurement_protocol_spec.rb
@@ -1,11 +1,25 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe GaMeasurementProtocol do
-  it "has a version number" do
+  it 'has a version number' do
     expect(GaMeasurementProtocol::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'Client' do
+    describe 'get'
+
+    describe '#post' do
+      subject { client.post(request) }
+
+      let(:client) { GaMeasurementProtocol::Client.new }
+      let(:request) do
+        {
+          tid: 'tid',
+          v: 'v'
+        }
+      end
+
+      it { is_expected.to be_a Hash }
+    end
   end
 end
